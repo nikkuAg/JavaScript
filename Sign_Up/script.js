f5bc06a943ac20ebbcd4428409433bba28b9d24d
Divyansh <a.divyansh.25@gmail.com>
2021-04-14 21:31:13 +0530

    Sign_Up form updated

--- Sign_Up/script.js
+++ Sign_Up/script.js
@@ -1,101 +1,213 @@
+var count = 0;
+var countName = 0;
+var countNumber = 0;
+var countCity = 0;
+var countEmail = 0;
+var countAge = 0;
+var countQualification = 0;
+var countPassword = 0;
+var countCPassword = 0;
 
 var input = document.getElementsByTagName("input")
-var username = document.getElementById("username");
+var username = document.getElementById("username")
 var age = document.getElementById("age")
 var male = document.getElementById("male");
 var female = document.getElementById("female");
 var number = document.getElementById("phone");
 var city = document.getElementById("city");
 var email = document.getElementById("email");
+var qualification = document.getElementById("qualification");
 var password = document.getElementById("password");
 var cpassword = document.getElementById("cpassword");
-var passwordvalue = document.getElementById("password").value;
-var confrmpass = document.getElementById("cpassword").value;
 
 
-password.oninput = function(event){
-    passwordvalue = document.getElementById("password").value;
-    //console.log("password");
-}
-function form1(){
-    
-username.oninvalid = function(event){
-    event.target.setCustomValidity('Enter a valid name eg.Abc Def or abc');
-}
-username.oninput = function(event){
-    event.target.setCustomValidity("");
-}
-age.oninvalid = function(event){
-    event.target.setCustomValidity('Enter a valid age.')
-}
-age.oninput = function(event){
-    event.target.setCustomValidity('')
-}
-male.oninvalid = function(event){
-    event.target.setCustomValidity('Select one option');
-}
-male.oninput = function(event){
-    event.target.setCustomValidity('');
-}
-female.oninvalid = function(event){
-    event.target.setCustomValidity('Select one option');
-}
-female.oninput = function(event){
-    event.target.setCustomValidity('');
-}
-number.oninvalid = function(event){
-    event.target.setCustomValidity('Enter a valid number eg. +91-9898989898 or 9898989898');
-}
-number.oninput = function(event){
-    event.target.setCustomValidity('');
-}
-city.oninvalid = function(event){
-    event.target.setCustomValidity('Enter a valid city name');
-}
-city.oninput = function(event){
-    event.target.setCustomValidity('');
-}
-email.oninvalid = function(event){
-    event.target.setCustomValidity('Enter a valid email');
-}
-email.oninput = function(event){
-    event.target.setCustomValidity('');
-}
-password.oninvalid = function(event){
-    var text= "Enter a password with minimum 8 characters.";
-    var text1 = "\nAtleast one lowercase character.";
-    var text2 = "\nAtleast one uppercase character.";
-    var text3 = "\nAtleast one number.";
-    var text4 = "\nAtleast one special character.";
-    event.target.setCustomValidity(text + text1 + text2 + text3 + text4);
-}
-password.oninput = function(event){
-    event.target.setCustomValidity('');
-    passwordvalue = document.getElementById("password").value;
-}
-cpassword.oninput = function(event){
-    confrmpass = document.getElementById("cpassword").value;
-    cpassword.setAttribute("isvalid", true);
-   // console.log("truei");
-   
-}
-    if(confrmpass != passwordvalue){
-        cpassword.oninvalid = cpassword.setCustomValidity("Password do not match");
-        confrmpass = "";
-        //console.log("if");
-        return false;
+username.oninput = function(){
+    var nameCheck = /^[A-Za-z .]+$/
+    if(nameCheck.test(username.value)){
+        countName =1;
+        document.getElementById("errorName").innerHTML = "";
+        document.getElementById("errorName").style.display = "none";
     }
-    else if(confrmpass == passwordvalue){
-        cpassword.setAttribute("isvalid", true);
-        //console.log("else if" + " " + confrmpass);
-        cpassword.oninvalid = cpassword.setCustomValidity("");
-        return false;
+    else if(username.value == ""){
+        countName =0;
+        document.getElementById("errorName").style.display = "inline";
+        document.getElementById("errorName").innerHTML = "Name cannot be empty";
     }
     else{
-       // console.log("else");
-       
+        countName =0;
+        document.getElementById("errorName").style.display = "inline";
+        document.getElementById("errorName").innerHTML = "Enter a valid name";
     }
-    //console.log("form");
-    return true;
+}
+number.oninput = function(){
+    var numberCheck = /^(\+91[- ])?[6|7|8|9][0-9]{9}$/
+    if(numberCheck.test(number.value)){
+        countNumber =1;
+        document.getElementById("errorNum").innerHTML = "";
+        document.getElementById("errorNum").style.display = "none";
+    }
+    else if(number.value == ""){
+        countNumber = 0;
+        document.getElementById("errorNum").style.display = "inline";
+        document.getElementById("errorNum").innerHTML = "Phone Number cannot be empty";
+    }
+    else{
+        countNumber = 0;
+        document.getElementById("errorNum").style.display = "inline";
+        document.getElementById("errorNum").innerHTML = "Enter a valid phone number. Eg: +91-8989898989 or +91 8989898989 or 8989898989";
+    }
+}
+city.oninput = function(){
+    var cityCheck = /^[A-Za-z .]+$/
+    if(cityCheck.test(city.value)){
+        countCity =1;
+        document.getElementById("errorCity").innerHTML = "";
+        document.getElementById("errorCity").style.display = "none";
+    }
+    else if(city.value == ""){
+        countCity =0;
+        document.getElementById("errorCity").style.display = "inline";
+        document.getElementById("errorCity").innerHTML = "City name cannot be empty";
+    }
+    else{
+        countCity =0;
+        document.getElementById("errorCity").style.display = "inline";
+        document.getElementById("errorCity").innerHTML = "Enter a valid city name";
+    }
+}
+email.oninput = function(){
+    var emailCheck = /^[a-zA-Z0-9]+[a-zA-Z0-9\W_]*[a-zA-Z0-9]+@[a-zA-Z]+(.com)$/
+    if(emailCheck.test(email.value)){
+        countEmail =1;
+        document.getElementById("errorEmail").innerHTML = "";
+        document.getElementById("errorEmail").style.display = "none";
+    }
+    else if(email.value == ""){
+        countEmail =0;
+        document.getElementById("errorEmail").style.display = "inline";
+        document.getElementById("errorEmail").innerHTML = "Email cannot be empty";
+    }
+    else{
+        countEmail =0;
+        document.getElementById("errorEmail").style.display = "inline";
+        document.getElementById("errorEmail").innerHTML = "Enter a valid email";
+    }
+}
+age.oninput = function(){
+    var ageCheck = /^[1-9][0-9]{0,1}$/
+    if(ageCheck.test(age.value)){
+        countAge =1;
+        document.getElementById("errorAge").innerHTML = "";
+        document.getElementById("errorAge").style.display = "none";
+    }
+    else if(age.value == ""){
+        countAge =0;
+        document.getElementById("errorAge").style.display = "inline";
+        document.getElementById("errorAge").innerHTML = "Age cannot be empty";
+    }
+    else{
+        countAge =0;
+        document.getElementById("errorAge").style.display = "inline";
+        document.getElementById("errorAge").innerHTML = "Enter a valid age";
+    }
+}
+password.oninput = function(){
+    var smallLetter = /[a-z]/;
+    var capitalLetter = /[A_Z]/;
+    var numberPassword = /[0-9]/;
+    var specialCharacter = /[\W_]/;
+    var x,y,z,a;
+    if(smallLetter.test(password.value)){
+        x=1;
+    }
+    else{
+        x=0;
+    }
+    if(capitalLetter.test(password.value)){
+        y=1;
+    }
+    else{
+        y=0;
+    }
+    if(numberPassword.test(password.value)){
+        z=1;
+    }
+    else{
+        z=0;
+    }
+    if(specialCharacter.test(password.value)){
+        a=1;
+    }
+    else{
+        a=0;
+    }
+    countPassword = x + y + z + a;
+    if(countPassword < 4 && password.value.length <8){
+        countPassword = 0;
+        document.getElementById("errorPassword").style.display = "inline";
+        document.getElementById("errorPassword").innerHTML = "Enter a valid password of minimum 8 characters. Password should contain atleast one small character, atleast one capital character, atleast one number and atleast one special character.";
+    }
+    else{
+        countPassword = 1;
+        document.getElementById("errorPassword").style.display = "none";
+    }     
+
+}
+cpassword.oninput = function(){
+    if(password.value == cpassword.value){
+        countCPassword = 1;
+        document.getElementById("errorCPassword").style.display = "none";
+    }
+    else{
+        countCPassword = 0;
+        document.getElementById("errorCPassword").style.display = "inline";
+        document.getElementById("errorCPassword").innerHTML = "Password are not matching";
+    }
+}
+qualification.oninput = function(){
+    if(qualification.value != ""){
+        countQualification = 0;
+        document.getElementById("errorAge").style.display = "inline";
+        document.getElementById("errorAge").innerHTML = "Select Qualification";
+    }
+    else{
+        countQualification = 1;
+        document.getElementById("errorAge").style.display = "none";
+        document.getElementById("errorAge").innerHTML = "";
+    }
+}
+male.oninput = function(){
+    if(male.checked){
+        document.getElementById("errorGender").style.display = "none";
+        document.getElementById("errorGender").innerHTML = "";
+    }
+}
+female.oninput = function(){
+    if(female.checked){
+        document.getElementById("errorGender").style.display = "none";
+        document.getElementById("errorGender").innerHTML = "";
+    }
+}
+function form1(){
+    count = countNumber + countName + countEmail + countCity + countAge + countQualification + countPassword + countCPassword;
+    if(male.checked || female.checked){
+        document.getElementById("errorGender").style.display = "none";
+        document.getElementById("errorGender").innerHTML = "";
+        if(count==8){
+            return true;
+        }
+        else{    
+            document.getElementById("x").style.display = "inline";
+            document.getElementById("x").innerHTML = "Fill all the details.";
+            return false;
+        }
+    }
+    else{
+        document.getElementById("errorGender").style.display = "inline";
+        document.getElementById("errorGender").innerHTML = "Select Gender";
+        return false;
+    }
+    
+    
 }
 
